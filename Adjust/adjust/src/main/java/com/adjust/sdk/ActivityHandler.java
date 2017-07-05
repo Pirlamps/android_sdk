2c073321f43b128f72832fce25fb7056701f681f
nonelse <pedro@adjust.com>
2017-07-05 10:08:40 +0200

    Remove delete saved referrer after checking

--- Adjust/adjust/src/main/java/com/adjust/sdk/ActivityHandler.java
+++ Adjust/adjust/src/main/java/com/adjust/sdk/ActivityHandler.java
@@ -783,12 +783,6 @@ public class ActivityHandler implements IActivityHandler {
 
         // send referrer
         sendReferrerI(referrer, clickTime);
-
-        // delete from preferences
-        SharedPreferences.Editor editor = settings.edit();
-        editor.remove(Constants.REFERRER_PREFKEY);
-        editor.remove(Constants.REFERRER_CLICKTIME_PREFKEY);
-        editor.apply();
     }
 
     private void processSessionI() {
